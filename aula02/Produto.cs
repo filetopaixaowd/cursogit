306c3efb9e141985fe2502976987bfa54293c13d
Fileto Paixão <filetopaixaowd@gmail.com>
2018-11-18 20:32:41 -0300

    Aula 02 curso C#

---
+++ aula02/Produto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace aula02
+{
+    class Produto
+    {
+        public string nome { get; private set; };
+        private double preco { get; private set; };
+        private int qtdEstoque { get; private set; };
+
+
+
+        public Produto(string nome, double preco , int qtdEstoque)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.qtdEstoque = qtdEstoque;
+        }
+
+        public Produto(string nome, double preco)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.qtdEstoque = 0;
+        }
+
+        public double valorTotalEmEstoque()
+        {
+            return preco * qtdEstoque;
+        }
+
+        public void realizarEntrada(int quantidade)
+        {
+            qtdEstoque = qtdEstoque + quantidade;
+        }
+
+        public void realizarSaida(int quantidade)
+        {
+            qtdEstoque = qtdEstoque - quantidade;
+        }
+
+        public override string ToString()
+        {
+            return nome 
+                + ", R$" 
+                + preco.ToString("F2",CultureInfo.InvariantCulture) 
+                + ", " 
+                + qtdEstoque
+                +" unidades, total em estoque: R$"
+                + valorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ aula02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace aula02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtd;
+            Produto P;
+
+                        Console.WriteLine("Digite os dados do produto:");
+
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preco: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture );
+
+            Console.Write("Qtd em estoque: ");
+            int qtdEstoque = int.Parse(Console.ReadLine());
+
+            P = new Produto(nome, preco, qtdEstoque);
+
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
